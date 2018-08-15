fa1f1ad8b190ff21f3dcc3064ce956b539ae2c5f
Xiang Xu <8851278+drxiang@users.noreply.github.com>
2018-08-15 23:22:37 +0200

    Exercise 1: Linear regression

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -13,7 +13,7 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-h = X * theta; % (theta' * X')' 
+h = X * theta; % (theta' * X')'
 
 A = h - y;
 
