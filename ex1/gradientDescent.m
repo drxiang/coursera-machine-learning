c6fb8d91728c13ddf421c3caff9b9115dde8c8a4
Xiang Xu <8851278+drxiang@users.noreply.github.com>
2018-08-16 10:49:41 +0200

    Exercise 1: Linear regression

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -22,7 +22,6 @@ for iter = 1:num_iters
     theta = theta - alpha / m * X' * (h-y); % ((h - y)' * X)'
 
 
-
     % ============================================================
 
     % Save the cost J in every iteration    
